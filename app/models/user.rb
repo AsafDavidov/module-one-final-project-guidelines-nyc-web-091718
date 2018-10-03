5009ac9b3bc2e0a3253a8946d22804e915599c93
AsafDavidov <adavidov@conncoll.edu>
2018-10-03 12:54:34 -0400

    started methods for user to return specific stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,20 @@ class User < ActiveRecord::Base
   has_many :games, through: :user_games
   has_many :stats, through: :games
 
+  def total(category)
+    self.games.map do |game|
+      game.stats[0][category]
+    end.reduce(:+)
+  end
+
+  def total_points
+    total(:points)
+  end
+
+  def total_rebounds
+    total(:rebounds)
+  end
+
+
 
 end
